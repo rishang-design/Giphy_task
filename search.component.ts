37a73fd9408ebfcfa01727680fb96576cc69b5cf
rishang-design <64736745+rishang-design@users.noreply.github.com>
2022-07-23 00:42:03 +0530

    Add files via upload

---
+++ search.component.ts
@@ -0,0 +1,28 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DataService} from '../data.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+
+  constructor(private dataService:DataService ) { 
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+  search(searchTerm: string){
+    if(searchTerm !==" "){
+      this.dataService.searchGifs(searchTerm);
+    }
+  };
+
+  
+
+}
